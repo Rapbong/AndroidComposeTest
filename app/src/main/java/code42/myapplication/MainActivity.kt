5ea6cb377ccbbb04cba06dc64f14cd6de5b8f5d6
ILBEOM KIM <ilbeom.kim@42dot.ai>
2020-08-09 20:40:53 +0900

    add preview

--- app/src/main/java/code42/myapplication/MainActivity.kt
+++ app/src/main/java/code42/myapplication/MainActivity.kt
@@ -2,14 +2,27 @@ package code42.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.Composable
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            Greeting("Android")
         }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text (text = "Hello $name")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android")
     }
 }
